495847586377c4e7a99ff35ceca09e23330acea4
Ahmedin Hasanovic <ahasanovic2@etf.unsa.ba>
2020-10-17 19:02:08 +0200

    Izmijenjen tekst ispisa

--- src/ba/unsa/etf/rpr/Main.java
+++ src/ba/unsa/etf/rpr/Main.java
@@ -3,6 +3,6 @@ package ba.unsa.etf.rpr;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Pozdrav svima koji gledaju video");
+	    System.out.println("Bile dvije budale, sada je došla treća budala.");
     }
 }
